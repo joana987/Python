59c2c686a628489ba10bd3421963cdb427243a51
joana987 <moreira.jmendes@gmail.com>
2025-08-04 17:00:27 +0100

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,6 @@
+salario = int(input("Quanto é que recebe por mês?"))
+horas = int(input("Quantas horas trabalha por mês?"))
+
+resultado = (salario/horas)
+print(f'O funcionário recebe {resultado} por hora')
+
